82d4f401bf7b91849e805de413064dfb3c85c166
wkyoshimatsu <101765683+wkyoshimatsu@users.noreply.github.com>
2024-10-21 14:24:55 -0300

    feat: Set project to terminal usage.

--- src/main/java/me/wky/screenmatch_terminal/ScreenmatchTerminalApplication.java
+++ src/main/java/me/wky/screenmatch_terminal/ScreenmatchTerminalApplication.java
@@ -1,10 +1,13 @@
 package me.wky.screenmatch_terminal;
 
+import me.wky.screenmatch_terminal.service.APIConsumer;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScreenmatchTerminalApplication {
+public class ScreenmatchTerminalApplication implements CommandLineRunner {
+	public static final String OMDBAPI_KEY= System.getenv("OMDBAPI_KEY");
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchTerminalApplication.class, args);
