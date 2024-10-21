4060fd20fc476e1bd2fa013a0278fe7c17503719
wkyoshimatsu <101765683+wkyoshimatsu@users.noreply.github.com>
2024-10-21 15:33:20 -0300

    fear: Added season conversion feature

--- src/main/java/me/wky/screenmatch_terminal/ScreenmatchTerminalApplication.java
+++ src/main/java/me/wky/screenmatch_terminal/ScreenmatchTerminalApplication.java
@@ -1,11 +1,16 @@
 package me.wky.screenmatch_terminal;
 
+import me.wky.screenmatch_terminal.model.EpisodeData;
+import me.wky.screenmatch_terminal.model.SeasonData;
 import me.wky.screenmatch_terminal.model.SerieData;
 import me.wky.screenmatch_terminal.service.APIConsumer;
 import me.wky.screenmatch_terminal.service.DataConverter;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchTerminalApplication implements CommandLineRunner {
@@ -25,5 +30,23 @@ public class ScreenmatchTerminalApplication implements CommandLineRunner {
 		var dataConverter = new DataConverter();
 		var serieData = dataConverter.convert(json, SerieData.class);
 		System.out.println(serieData);
+
+		url = "http://www.omdbapi.com/?apikey=" + OMDBAPI_KEY + "&t=Friends&Season=1&Episode=1";
+		json = apiConsumer.getJsonByUrl(url);
+		var episodeData = dataConverter.convert(json, EpisodeData.class);
+		System.out.println(episodeData);
+
+		List<SeasonData> seasons = new ArrayList<>();
+
+		for (int i = 1; i <= serieData.totalSeasons(); i++) {
+			url = "http://www.omdbapi.com/?apikey=" + OMDBAPI_KEY + "&t=Friends&Season=" + i;
+			json = apiConsumer.getJsonByUrl(url);
+			var seasonData = dataConverter.convert(json, SeasonData.class);
+			seasons.add(seasonData);
+		}
+
+		for (var season : seasons) {
+			System.out.println(season);
+		}
 	}
 }
