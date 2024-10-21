16268e9144d49666406f6adb5f258c170d0c5810
wkyoshimatsu <101765683+wkyoshimatsu@users.noreply.github.com>
2024-10-21 14:03:34 -0300

    Criando projeto Spring

---
+++ src/main/java/me/wky/screenmatch_terminal/ScreenmatchTerminalApplication.java
@@ -0,0 +1,13 @@
+package me.wky.screenmatch_terminal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchTerminalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchTerminalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/wky/screenmatch_terminal/ScreenmatchTerminalApplicationTests.java
@@ -0,0 +1,13 @@
+package me.wky.screenmatch_terminal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenmatchTerminalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
