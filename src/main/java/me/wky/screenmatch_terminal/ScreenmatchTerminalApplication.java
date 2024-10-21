79398ab7a0e4749762d4d83a2f6435fb8a2c5da0
wkyoshimatsu <101765683+wkyoshimatsu@users.noreply.github.com>
2024-10-21 16:07:07 -0300

    refactor: Moved terminal features to terminal class

--- src/main/java/me/wky/screenmatch_terminal/ScreenmatchTerminalApplication.java
+++ src/main/java/me/wky/screenmatch_terminal/ScreenmatchTerminalApplication.java
@@ -1,16 +1,9 @@
 package me.wky.screenmatch_terminal;
 
-import me.wky.screenmatch_terminal.model.EpisodeData;
-import me.wky.screenmatch_terminal.model.SeasonData;
-import me.wky.screenmatch_terminal.model.SerieData;
-import me.wky.screenmatch_terminal.service.APIConsumer;
-import me.wky.screenmatch_terminal.service.DataConverter;
+import me.wky.screenmatch_terminal.terminal.Terminal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchTerminalApplication implements CommandLineRunner {
@@ -22,31 +15,7 @@ public class ScreenmatchTerminalApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var url = "http://www.omdbapi.com/?apikey=" + OMDBAPI_KEY + "&t=Friends";
-		var apiConsumer = new APIConsumer();
-		var json = apiConsumer.getJsonByUrl(url);
-		System.out.println(json);
-
-		var dataConverter = new DataConverter();
-		var serieData = dataConverter.convert(json, SerieData.class);
-		System.out.println(serieData);
-
-		url = "http://www.omdbapi.com/?apikey=" + OMDBAPI_KEY + "&t=Friends&Season=1&Episode=1";
-		json = apiConsumer.getJsonByUrl(url);
-		var episodeData = dataConverter.convert(json, EpisodeData.class);
-		System.out.println(episodeData);
-
-		List<SeasonData> seasons = new ArrayList<>();
-
-		for (int i = 1; i <= serieData.totalSeasons(); i++) {
-			url = "http://www.omdbapi.com/?apikey=" + OMDBAPI_KEY + "&t=Friends&Season=" + i;
-			json = apiConsumer.getJsonByUrl(url);
-			var seasonData = dataConverter.convert(json, SeasonData.class);
-			seasons.add(seasonData);
-		}
-
-		for (var season : seasons) {
-			System.out.println(season);
-		}
+		Terminal terminal = new Terminal();
+		terminal.printMenu();
 	}
 }
---
+++ src/main/java/me/wky/screenmatch_terminal/terminal/Terminal.java
@@ -0,0 +1,48 @@
+package me.wky.screenmatch_terminal.terminal;
+
+import me.wky.screenmatch_terminal.model.SeasonData;
+import me.wky.screenmatch_terminal.model.SerieData;
+import me.wky.screenmatch_terminal.service.APIConsumer;
+import me.wky.screenmatch_terminal.service.DataConverter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Terminal {
+    private final String OMDBAPI_KEY= System.getenv("OMDBAPI_KEY");
+
+    private final String API_URL = "http://www.omdbapi.com/?apikey=" + OMDBAPI_KEY + "&t=";
+
+    private Scanner scanner = new Scanner(System.in);
+
+    private APIConsumer apiConsumer = new APIConsumer();
+
+    private DataConverter dataConverter = new DataConverter();
+
+    public void printMenu() {
+        System.out.println("Digite o nome da série desejada:");
+        var serieName = scanner.nextLine().toLowerCase().replace(" ", "+");
+
+        var url = API_URL + serieName;
+        var json = apiConsumer.getJsonByUrl(url);
+        var serieData = dataConverter.convert(json, SerieData.class);
+        System.out.println(serieData);
+
+        List<SeasonData> seasons = new ArrayList<>();
+
+        for (int i = 1; i <= serieData.totalSeasons(); i++) {
+            url = API_URL + serieName + "&Season=" + i;
+            json = apiConsumer.getJsonByUrl(url);
+            var seasonData = dataConverter.convert(json, SeasonData.class);
+            seasons.add(seasonData);
+        }
+
+        for (var season : seasons) {
+            System.out.println(season);
+        }
+
+    }
+
+
+}
