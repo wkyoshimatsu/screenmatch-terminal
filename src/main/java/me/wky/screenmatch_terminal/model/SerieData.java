2f2b42fa7601ba541976af0fef29b512d2ffcf19
wkyoshimatsu <101765683+wkyoshimatsu@users.noreply.github.com>
2024-10-21 14:49:05 -0300

    fix: Unused JSON properties bugfix

--- src/main/java/me/wky/screenmatch_terminal/model/SerieData.java
+++ src/main/java/me/wky/screenmatch_terminal/model/SerieData.java
@@ -1,7 +1,9 @@
 package me.wky.screenmatch_terminal.model;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record SerieData(@JsonAlias("Title") String title,
                         Integer totalSeasons,
                         @JsonAlias("imdbRating")String rating) {
