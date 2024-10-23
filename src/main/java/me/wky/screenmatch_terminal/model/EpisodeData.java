2ee6fc2ac8aaf467ff9801e9d1494652b345bfc2
wkyoshimatsu <101765683+wkyoshimatsu@users.noreply.github.com>
2024-10-23 15:15:48 -0300

    refactor: Changed variables names

--- src/main/java/me/wky/screenmatch_terminal/model/EpisodeData.java
+++ src/main/java/me/wky/screenmatch_terminal/model/EpisodeData.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record EpisodeData(@JsonAlias("Title") String title,
-                          @JsonAlias("Episode") Integer episodeNumber,
+                          @JsonAlias("Episode") Integer episode,
                           @JsonAlias("imdbRating") String rating,
                           @JsonAlias("Released") String released) {
 }
--- src/main/java/me/wky/screenmatch_terminal/model/SeasonData.java
+++ src/main/java/me/wky/screenmatch_terminal/model/SeasonData.java
@@ -6,6 +6,6 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record SeasonData(@JsonAlias("Season") Integer seasonNumber,
+public record SeasonData(@JsonAlias("Season") Integer season,
                          @JsonAlias("Episodes") List<EpisodeData> episodes) {
 }
